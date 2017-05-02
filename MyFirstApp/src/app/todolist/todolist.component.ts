72841dc1ad38efc0bf0b0978d5edc6280d859da5
Kamil Brozek <04kamil@gmail.com>
2017-05-02 19:30:18 +0200

    and next

--- MyFirstApp/src/app/todolist/todolist.component.ts
+++ MyFirstApp/src/app/todolist/todolist.component.ts
@@ -9,9 +9,42 @@ export class TodolistComponent {
   title = 'to do list!!!';
   task:string='';
 
+todos:Array<ToDo>=[
+{
+  title: "nisko",
+  isMy: true
+},
+{
+  title: 'angular',
+  isMy: false
+}
+
+];
+
   addToDo()
   {
-    console.log(this.task);
-    
+    if(!this.task)
+    {
+      return;
+    }
+    const newTask:ToDo={
+      title:this.task,
+      isMy:true
+    };
+    this.todos.push(newTask);
+    this.task= '';
   }
+
+  deleteToDo(index)
+  {
+    this.todos.splice(index,1);
+  }
+
+
 }
+
+interface ToDo{
+  title:string;
+  isMy:boolean;
+}
+
