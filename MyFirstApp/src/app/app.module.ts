a2f6f478d70d945a624ce1a16813f2fd62631bdf
Kamil Brozek <04kamil@gmail.com>
2017-05-03 23:10:02 +0200

    and next

--- MyFirstApp/src/app/app.module.ts
+++ MyFirstApp/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { TodolistComponent } from './todolist/todolist.component';
 import { WWWComponent } from './www/www.component';
 import { CodeComponent } from './code/code.component';
+import { HistoryComponent } from './history/history.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodolistComponent,
     WWWComponent,
-    CodeComponent
+    CodeComponent,
+    HistoryComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
